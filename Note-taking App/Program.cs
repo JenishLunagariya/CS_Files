12570c405fe6d9fb835d65704d02fd8670b3aaf3
JenishLunagariya <jenishlunagariya71@gmail.com>
2023-03-14 14:28:46 +0530

    updated program.cs

--- Note-taking App/Program.cs
+++ Note-taking App/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace myApplication{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
