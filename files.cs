47d961cb20e869347d7998ae15cae130d4561fc7
JenishLunagariya <jenishlunagariya71@gmail.com>
2023-03-15 16:13:27 +0530

    tried crytography but getting errors

--- files.cs
+++ files.cs
@@ -1,18 +1,24 @@
 using System;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace myFileIO{
     class Program{
         static void Main(string[] args){
-            string writeTxt = "Hello World!";
-            File.WriteAllText("files.txt",writeTxt);
-            string readTxt = File.ReadAllText("files.txt");
-            Console.WriteLine(readTxt);
+            byte[] key = {0x02, 0x03, 0x01, 0x03, 0x03, 0x07, 0x07, 0x08, 0x09, 0x09, 0x11, 0x11, 0x16, 0x17, 0x19, 0x16};
+            try{
+                using FileStream myStream = new FileStream(@"C:\Users\10710516\OneDrive - LTI\Desktop\JBL\CS_Files\files.txt",FileMode.OpenOrCreate);
 
-            writeTxt = "Password";
-            File.WriteAllText("files.txt",writeTxt);
-            readTxt = File.ReadAllText("files.txt");
-            Console.WriteLine(readTxt);
+                using Aes aes = Aes.Create();
+                aes.Key = key;
+
+            }catch{
+                Console.WriteLine("Error");
+                throw;
+            }
+            
+            Console.WriteLine("Running...");
+
         }
     }
 }
