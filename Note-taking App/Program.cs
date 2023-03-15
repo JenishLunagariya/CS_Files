10b75c599f7dd7c5963ba9a79cedb05496cc7720
JenishLunagariya <jenishlunagariya71@gmail.com>
2023-03-15 10:13:07 +0530

    login code to generate password and user txt file

--- Note-taking App/Program.cs
+++ Note-taking App/Program.cs
@@ -3,52 +3,69 @@ using System;
 using System.IO;
 namespace myApplication{
     class Login{
+        // string loginpass = File.ReadAllText("password.txt");
         public string LoginPassword{
             get;set;
         }
         public string LoginUsername{
             get;set;
         }
-        
+
+        public Login(){
+            string writeuserName = "Jenish";
+            // string writepassWord = "12345678";
+
+            // File.WriteAllText("password.txt",writepassWord);
+            File.WriteAllText("username.txt",writeuserName);
+            this.LoginPassword = File.ReadAllText("password.txt");
+            this.LoginUsername = File.ReadAllText("username.txt");
+        }
+        public void setLoginPassword(string password){
+            File.WriteAllText("password.txt",password);
+        }
+        public string getLoginPassword(){
+            LoginPassword = File.ReadAllText("password.txt");
+            return LoginPassword;
+        }   
     }
     class Program:Login{
 
         static void Main(string[] args){
             Console.WriteLine("Hello, Please enter your login credentials");
+            Login loginCredentials = new Login();
+           
             Console.Write("username: ");
             string userName = Console.ReadLine();
             Console.Write("password: ");
             string passWord = Console.ReadLine();
-            Console.Write("User is "+userName+" with password "+passWord+"\n");
-            Login loginCredentials = new Login();
-            loginCredentials.LoginUsername = "Jen";
-            loginCredentials.LoginPassword = "123";
             string loginuserName = loginCredentials.LoginUsername;
             string loginuserPassword = loginCredentials.LoginPassword;
-            // Console.WriteLine(loginuserName==null);
+
+            // check login credentials;
             if (userName == loginuserName && passWord == loginuserPassword){
                 Console.WriteLine("Credentials are correct!");
-            }else{
-                Console.WriteLine("username or password is incorrect!");
-            }
-            Console.WriteLine("Do you want to change password? ");
-            string answer = Console.ReadLine();
-            if (answer == "y"){
+
+                Console.WriteLine("Do you want to change password? ");
+                string answer = Console.ReadLine();
+                if (answer == "y"){
                 Console.WriteLine("Enter old password: ");
                 string oldpassWord = Console.ReadLine();
                 if (oldpassWord == loginuserPassword){
                     Console.WriteLine("Enter new Password: ");
                     string newpassWord = Console.ReadLine();
-                    loginCredentials.LoginPassword = newpassWord;
+                    loginCredentials.setLoginPassword(newpassWord);
+                    // loginCredentials.LoginPassword = newpassWord;
                     Console.WriteLine("Password is updated!");
                 }else{
                     Console.WriteLine("Old password is wrong.");
                 }
-                loginuserPassword = loginCredentials.LoginPassword;
+                loginuserPassword = loginCredentials.getLoginPassword();
                 Console.WriteLine("Current password: "+loginuserPassword);
+                
             }
-            
-
+            }else{
+                Console.WriteLine("username or password is incorrect!");
+            }
         }
     }
 }
